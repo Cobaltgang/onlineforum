c43632461407db98ccfb67d569f6f389f291f5c5
Ashish Kumar Singh <blr.ashu@gmail.com>
2019-12-17 16:03:52 +0000

    Added Dependency

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+// Require composers autoloader
+require_once './vendor/autoload.php';
+
+// Initiate a new instance of the Pwned class
+$pwned = new \MFlor\Pwned\Pwned();
+echo $pwned->passwords()->occurences('gfhgfHDhfdgf5656JKG');
+
+?>
