5ef14b727593ca9abfbf00d6f28a662e6db6742e
Ashish Kumar Singh <blr.ashu@gmail.com>
2019-12-18 17:58:20 +0000

    added index page

--- logout.php
+++ logout.php
@@ -2,20 +2,15 @@
 include_once('functions.php');
 include_once('config.php');
 session_start();
-
 $serial = $_COOKIE['serial'];
 $token = $_COOKIE['token'];
-
-
 func::deleteRecord($dbh, $serial, $token);
-            
-
-
  unset($_SESSION['username']);
  unset($_SESSION['serial']);
  unset($_SESSION['token']);
  unset($_SESSION['user_id']);
  session_destroy();
  func::deleteCookie();
+ header('location: forms.php')
 
  ?>
