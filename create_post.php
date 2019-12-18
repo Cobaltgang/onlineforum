ecb8fb0840ff2fbb672cb63c953f11c09889c08e
Ashish Kumar singh <blr.ashu@gmail.com>
2019-12-18 23:04:17 +0000

    Created prototype

--- create_post.php
+++ create_post.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['username'])) {
   <title>NCI Forum</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/blog-home.css" rel="stylesheet">
